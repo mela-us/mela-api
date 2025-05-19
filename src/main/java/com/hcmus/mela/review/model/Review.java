3f121feba1b6da9c54bd95570a8ff4a8cd97014c
thinant <nguynthinan@outlook.com>
2025-05-19 18:37:53 +0700

    build review model

---
+++ src/main/java/com/hcmus/mela/review/model/Review.java
@@ -0,0 +1,37 @@
+package com.hcmus.mela.review.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "reviews")
+public class Review {
+    @Id
+    @Field(name = "_id")
+    private UUID reviewId;
+
+    @Field(name = "user_id")
+    private UUID userId;
+
+    @Field(name = "type")
+    private ReviewType reviewType;
+
+    @Field(name = "created_at")
+    private Date createdAt;
+
+    @Field(name = "exercise_list")
+    private List<ExerciseReference> exerciseList;
+
+    @Field(name = "section_list")
+    private List<SectionReference> sectionList;
+}
