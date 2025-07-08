75abcae82f229034c1b5a52646b2f45cb14bc969
TEIKHNA <anhkiet07012003@gmail.com>
2025-07-08 21:06:55 +0700

    fix: rejectReason to rejectedReason

--- src/main/java/com/hcmus/mela/exercise/dto/dto/ExerciseDto.java
+++ src/main/java/com/hcmus/mela/exercise/dto/dto/ExerciseDto.java
@@ -27,5 +27,5 @@ public class ExerciseDto {
 
     private UUID createdBy;
 
-    private String rejectReason;
+    private String rejectedReason;
 }
--- src/main/java/com/hcmus/mela/topic/dto/dto/TopicDto.java
+++ src/main/java/com/hcmus/mela/topic/dto/dto/TopicDto.java
@@ -20,7 +20,7 @@ public class TopicDto {
 
     private UUID createdBy;
 
-    private String rejectReason;
+    private String rejectedReason;
 
     private ContentStatus status;
 }
